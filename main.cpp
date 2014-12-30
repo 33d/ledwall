1ecf300770225375bbb8cfdb58b72d411e12f4b7
Damien <damien@sail>
2014-12-30 19:06:23 +1030

    Periodically change effect

--- main.cpp
+++ main.cpp
@@ -9,20 +9,42 @@
 #define COLS 12
 #define DOTS 20
 
+#define EFFECT_TIME (300 * 1000)
+
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(ROWS*COLS, PIN, NEO_GRB + NEO_KHZ800);
 
 Pattern *effect;
+unsigned long last_change;
+
+void next_effect() {
+  static uint8_t id = 0;
+  delete effect;
+  pixels.clear();
+  switch (id) {
+  case 0: effect = new Snake(pixels, COLS, ROWS); break;
+  case 1: effect = new Matrix(pixels, COLS, ROWS); break;
+  }
+  effect->init();
+  id = ++id % 2;
+}
 
 void setup() {
   pixels.begin();
   pixels.show();
 
+  next_effect();
   effect = new Snake(pixels, COLS, ROWS);
   effect->init();
+
+  last_change = millis();
 }
 
 void loop() {
+  if (millis() - last_change > EFFECT_TIME) {
+    next_effect();
+    last_change = millis();
+  }
+
   effect->step();
   pixels.show();
 }
-
