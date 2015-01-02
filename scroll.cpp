068fe5eca94fa2bc0f18153bd64a41ffbcf09bbb
Damien <damien@sail>
2015-01-02 15:05:39 +1030

    Fixed the issue with the jumpy scrolling at the start

--- scroll.cpp
+++ scroll.cpp
@@ -7,29 +7,32 @@
 #define WIDTH pattern_height
 
 void Scroll::init() {
-  pos = -(int16_t) cols*sub;
+  pos = 0;
 }
 
 void Scroll::step() {
-  delay(5);
+  delay(1);
 
   // How much to contribute to the first column
-  // The signed modulo here is platform dependent!
   uint8_t first_val = 255-((pos % sub) * (256 / sub));
   for (uint8_t col = 0; col < cols; col++) {
-    int8_t bitmapx = pos / sub + col;
-    for (uint8_t y = 0, mask = 1; y < 8; y++, mask <<= 1) {
-      bool first = bitmapx >= 0 && bitmapx < WIDTH && (BITMAP[bitmapx] & mask);
-      bool second = bitmapx >= -1 && bitmapx < WIDTH-1 && (BITMAP[bitmapx+1] & mask);
-      uint8_t val = first && !second ? first_val
-        : !first && second ? (255-first_val)
-        : first && second ? 255
-        : 0;
-      plot(col, y, val, val, val);
+    // This will skip the fading effect on the first frame, but it's hard
+    // to notice
+    if (pos / sub + col >= cols) {
+      uint8_t bitmapx = pos / sub - cols + col;
+      for (uint8_t y = 0, mask = 1; y < 8; y++, mask <<= 1) {
+        bool first = bitmapx >= 0 && bitmapx < WIDTH && (BITMAP[bitmapx] & mask);
+        bool second = bitmapx >= -1 && bitmapx < WIDTH-1 && (BITMAP[bitmapx+1] & mask);
+        uint8_t val = first && !second ? first_val
+          : !first && second ? (255-first_val)
+          : first && second ? 255
+          : 0;
+        plot(col, y, val, val, val);
+      }
     }
   }
 
   ++pos;
-  if (pos >= ((int16_t) WIDTH + cols) * sub)
+  if (pos >= ((uint16_t) WIDTH + cols) * sub + gap)
     init();
 }
--- scroll.h
+++ scroll.h
@@ -5,11 +5,12 @@
 
 class Scroll: public Pattern {
   // How many levels of "sub-frames" to render
-  static const int8_t sub = 8;
-  int16_t pos;
+  static const uint8_t sub = 8;
+  uint16_t pos;
+  const uint16_t gap;
 public:
   Scroll(Adafruit_NeoPixel& pixels, uint8_t cols, uint8_t rows)
-    : Pattern(pixels, cols, rows), pos(0) {}
+    : Pattern(pixels, cols, rows), pos(0), gap(cols*sub) {}
   void init();
   void step();
   virtual ~Scroll() {};
