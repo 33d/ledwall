853a8ea3f709d6f3cf54a39dae27e18eecfc32a9
Damien <damien@sail>
2015-11-20 05:12:36 +1030

    Change the effect duration from 5 to 3 minutes

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@
 #define COLS 12
 #define DOTS 20
 
-#define EFFECT_TIME ((uint32_t) 300 * 1000)
+#define EFFECT_TIME ((uint32_t) 180 * 1000)
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(ROWS*COLS, PIN, NEO_GRB + NEO_KHZ800);
 
